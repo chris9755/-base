0942938eb5e19e3a848d201044415662902f6a5c
Krzysztof <gipson.hose@gmail.com>
2023-04-09 18:43:09 +0200

    add js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+const nav = document.querySelector('.navbar')
+
+function addShadow(){
+    if(window.scrollY >=150){
+        nav.classList.add('shadow-bg')
+    }else{
+        nav.classList.remove('shadow-bg')
+    }
+
+    
+}
+window.addEventListener('scroll', addShadow)
+})
 
 
