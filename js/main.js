89654caca37a7b8b441ebc068cf2e9d6bebe2020
Krzysztof <gipson.hose@gmail.com>
2023-04-22 18:36:22 +0200

    add cookies

--- js/main.js
+++ js/main.js
@@ -5,3 +5,25 @@ document.addEventListener('click', () => {
     nav.classList.remove('show')
   }
 })
+
+
+
+const cookieBox = document.querySelector('.cookie-box')
+const cookieBtn = document.querySelector('.cookie-btn')
+
+
+const showCookie = () => {
+	const cookieEaten = localStorage.getItem('cookie')
+	
+	if (cookieEaten) {
+		cookieBox.classList.add('hide')
+	}
+}
+
+const handleCookieBox = () => {
+	localStorage.setItem('cookie', 'true')
+	cookieBox.classList.add('hide')
+}
+
+cookieBtn.addEventListener('click', handleCookieBox)
+showCookie()
